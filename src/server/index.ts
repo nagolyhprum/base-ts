41973c98fbaaea4d9c97d1fc2363c7a578946c7c
Logan Murphy <nagolyhprum@hotmail.com>
2023-09-06 17:16:50 -0400

    initial commit

---
+++ src/server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const index = `<!doctype html>
+<html>
+    <body>
+        <script src="main.js"></script>
+    </body>
+</html>`;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+app.get("/", (_, res) => {
+	res.send(index);
+});
+
+app.listen(80);
